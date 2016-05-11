a3fde59bce33b4250538f381c26e84a3cb7afeba
Joao Holanda <joaoholandajr@gmail.com>
2016-05-11 14:02:39 -0300

    Adding scrapper

--- scrapper-apps/scrapper.js
+++ scrapper-apps/scrapper.js
@@ -1,9 +1,26 @@
 var gplay = require('google-play-scraper');
- 
-gplay.app({appId: 'com.dxco.pandavszombies'})
-  .then(function(app){
-    console.log('Retrieved application: ' + app.icon);
-  })
-  .catch(function(e){
-    console.log('There was an error fetching the application!');
-  });
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('teste')
+});
+
+lineReader.on('line', function (line) {
+  if(line != '}' && line != '{'){
+    var vals = line.split(" : ");
+
+    var pattern = /[\"\\]/g
+
+    console.log("********************")
+    console.log(vals[1])
+    console.log(vals[1].replace(pattern, ""))
+
+  }
+});
+
+// gplay.app({appId: 'com.dxco.pandavszombies'})
+//   .then(function(app){
+//     console.log('Retrieved application: ' + app.icon);
+//   })
+//   .catch(function(e){
+//     console.log('There was an error fetching the application!');
+//   });
